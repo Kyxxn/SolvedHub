e310e80dc27e9b57c3eb698dee4d885786ea1086
박효준 <gywns626@naver.com>
2024-07-04 05:29:32 +0900

    [Bronze II] Title: 음계, Time: 12 ms, Memory: 79520 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+let input = readLine()!.split(separator: " ").map {Int($0)!}
+
+if input == input.sorted(by: <) {
+    print("ascending")
+} else if input == input.sorted(by: >) {
+    print("descending")
+} else {
+    print("mixed")
+}
